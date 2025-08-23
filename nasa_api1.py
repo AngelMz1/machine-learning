c9e8c03ba4a7f4d4684212cf254a670f3cf7a259
Angel Muñoz <munozbenavidesangel@gmail.com>
2025-08-22 21:00:23 -0500

    ML – Challenge 1

--- nasa_api1.py
+++ nasa_api1.py
@@ -3,9 +3,7 @@ Dev. Angel muñoz
 Script description
 Get and read data from nasa API about space
 NAsa Api: https://api.nasa.gov/
-https://api.nasa.gov/neo/rest/v1/neo/3726712?api_key={FA3OhznLAkrWIdOn1jiCvS6F2cc7zuJMKtj6T7dr}
-'''
-
+https://api.nasa.gov/neo/rest/v1/neo/3726712?api_key={FA3OhznLAkrWIdOn1jiCvS6F2cc7zuJMKtj6T7dr}'''
 import os
 import requests
 
@@ -20,7 +18,15 @@ def get_nasa_data(API_KEY):
         response.raise_for_status()
         #read data
         data = response.json()
-        print(data)
+        #challenge api nasa
+        name = data.get("name")
+        abs_magnitude = data.get("absolute_magnitude_h")
+        diam_km = data["estimated_diameter"]["kilometers"]["estimated_diameter_max"]
+        diam_ft = data["estimated_diameter"]["feet"]["estimated_diameter_max"]
+        print(f"Nombre del cometa: {name}")
+        print(f"Magnitud absoluta h: {abs_magnitude}")
+        print(f"Diámetro máximo estimado (km): {diam_km}")
+        print(f"Diámetro máximo estimado (ft): {diam_ft}")
     except requests.exceptions.RequestException as e:
         print(f'Error: {e}')
             
