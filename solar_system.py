3852da6236b5e7195501f13f3774c84f315fdeab
Angel Muñoz <munozbenavidesangel@gmail.com>
2025-08-23 00:14:32 -0500

    ML – Challenge 2.2

--- solar_system.py
+++ solar_system.py
@@ -12,49 +12,79 @@ def getData():
         response.raise_for_status()
         #Object to JSON (JS Object Notation)
         data = response.json()
-        print(data)
+        return data
     except requests.exceptions.RequestException as e:
         print(f"API error: {e}")
 
 def showPlanets():
-    url = "https://api.le-systeme-solaire.net/rest/bodies/"
-    data = requests.get(url).json()
+    data = getData()
     print("\n::: PLANETS :::")
-    for b in data["bodies"]:
-        if b["isPlanet"]:
-            print("-", b["englishName"])
+    planets= [b for b in data["bodies"] if b["isPlanet"] and b["sideralOrbit"] is not None]
+    sortedPlanets = sorted(planets, key=lambda p: p["sideralOrbit"])
+    for b in sortedPlanets:
+        print("-", b["englishName"])
 
 def showMoons():
-    url = "https://api.le-systeme-solaire.net/rest/bodies/"
-    data = requests.get(url).json()
+    data = getData()
+    moonsByPlanet = {}
     print("\n::: MOONS :::")
     for b in data["bodies"]:
         if b.get("aroundPlanet"):
+            planetName = b["aroundPlanet"]["planet"]
+            moonName = b["englishName"]
+            if planetName not in moonsByPlanet:
+                moonsByPlanet[planetName] = []
+            moonsByPlanet[planetName].append(moonName)
+    for planet, moons in moonsByPlanet.items():
+        print(f"{planet}:")
+        for moon in moons:
+            print(f"- {moon}")
+
+def showStars():
+    data = getData()
+    print("\n::: STARS :::")
+    for b in data.get("bodies", []):
+        if b.get("bodyType") == "Star":
+            print("-", b["englishName"])
+
+def showAsteroids():
+    data = getData()
+    print("\n::: ASTEROIDS :::")
+    for b in data.get("bodies", []):
+        if b.get("bodyType") == "Asteroid":
             print("-", b["englishName"])
 
 def showComets():
-    url = "https://api.le-systeme-solaire.net/rest/bodies/?filter[]=isComet,eq,true"
-    data = requests.get(url).json()
+    data = getData()
     print("\n::: COMETS :::")
     for b in data.get("bodies", []):
-        print("-", b["englishName"])
+        if b.get("bodyType") == "Comet":
+            print("-", b["englishName"])
 
 def mainMenu():
     while True:
-        print("\n:::: MAIN MENU ::::")
-        print("1. Planetas")
-        print("2. Satélites")
-        print("3. Cometas")
-        print("4. Salir")
+        screen = '''
+:::: SOLAR SYSTEM MENU ::::
+    [1]. Planets
+    [2]. Moons
+    [3]. Stars
+    [4]. Asteroids
+    [5]. Comets
+    [6]. Salir'''
+        print(screen)
 
-        choice = input("Seleccione una opción: ")
-        if choice == "1":
+        option = input("Choose an option: ")
+        if option == "1":
             showPlanets()
-        elif choice == "2":
+        elif option == "2":
             showMoons()
-        elif choice == "3":
+        elif option == "3":
+            showStars()
+        elif option == "4":
+            showAsteroids()
+        elif option == "5":
             showComets()
-        elif choice == "4":
+        elif option == "6":
             print("Saliendo...")
             break
         else:
